b2d9db044a4766469c2ae1052ae1dfaba4f83ee3
Adina <yna_dulci@yahoo.com>
2023-07-10 13:05:18 +0100

    finished webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,60 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+// HtmlWebpackPlugin for generating the HTML file
+new HtmlWebpackPlugin({
+  template: "./index.html",
+  title: "Just Another Text Editor",
+}),
+
+// WebpackPwaManifest for generating the manifest file
+new WebpackPwaManifest({
+  fingerprints: false,
+  inject: true,
+  name: "Just Another Text Editor",
+  short_name: "J.A.T.E.",
+  description: "Take notes or write code snippets anywhere, anytime!",
+  background_color: "#272822",
+  theme_color: "#31A9E1",
+  start_url: "/",
+  publicPath: "./",
+  icons: [
+    {
+      src: path.resolve("src/images/logo.png"),
+      sizes: [96, 128, 192, 256, 384, 512],
+      destination: path.join("assets", "icons"),
+    },
+  ],
+}),
+
+// InjectManifest to generate service worker using Workbox
+new InjectManifest({
+  swSrc: "./src-sw.js",
+  swDest: 'src-sw.js',
+}),
+
       
     ],
 
     module: {
       rules: [
+        // CSS loaders to handle CSS files
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        }, 
         
       ],
     },
